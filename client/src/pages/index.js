607165822ab60b0505bf56202ba6814c2c891937
Ravi <knuts839@umn.edu>
2020-11-29 12:45:04 -0600

    my first page of application

---
+++ client/src/pages/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import BookList from '../../components/BookList'; 
+import API from '../../utils/API';
+
+class Bookshelf extends Component { 
+
+    state = {
+        books : []
+          // {
+          //   authors     : [],
+          //   _id         : "",
+          //   title       : '',
+          //   description : "",
+          //   image       : "",
+          //   link        : "" 
+          // }
+      };
+
+    
+    
+      //After component is mounted in React's DOM, populate the book list
+    componentDidMount() {
+      this.getSavedBooks();
+    }
+    
+    //Custom method to retrieve all saved books in the db.
+    getSavedBooks = () => {
+      API.getBooks().then(res => { 
+        res.data.map( (book, index) => { 
+          console.log("Bookshelf[33]" + JSON.stringify(book, '', 2));
+        })
+        this.setState( { books: res.data } )
+      })
+      .catch(err => console.log(err));
+    }
+
+    deleteBook = (bookId) => {
+
+      console.log('Delete Button clicked. Book id :' + bookId);  
+    
+      if ( bookId ) {
+    
+        API.deleteBook(bookId)
+        .then(res => {
+        console.log("Book deleted: " + res.data);
+        this.getSavedBooks();
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      }
+    }
+    
+
+
+    render() { 
+
+        return( 
+          <div className="section no-pad-bot" id="index-banner">
+          <div className="container">
+            <div className="row center">
+              <BookList books={this.state.books } caller="bookshelf" onDelete={this.deleteBook}/>
+            </div>
+          </div>
+        </div> 
+        );
+    }
+}
+
+export default Bookshelf;
